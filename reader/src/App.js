776048f7acd004e1ba32a1f00cd14a0d434718b2
Andrew Boza <andrew.m.boza@gmail.com>
2019-04-17 19:18:33 -0400

    Makes Reader compilable

--- reader/src/App.js
+++ reader/src/App.js
@@ -1,35 +1,9 @@
 import React, { Component } from 'react';
-import Reader from './reader';
-import Editor from './editor';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    currentApp: 'Reader',
-  }
-
-  goToReader = () => {
-    this.setState({
-      currentApp: 'Reader',
-    });
-  }
-
-  goToEditor = () => {
-    this.setState({
-      currentApp: 'Editor',
-    });
-  }
-
   render() {
-    return (
-      <div className='App'>
-        <a onClick={this.goToReader}> Go To Reader </a>
-        <a onClick={this.goToEditor}> Go To Editor </a>
-        {this.state.currentApp === 'Reader' && <Reader />}
-        {this.state.currentApp === 'Editor' && <Editor />}
-      </div>
-    );
+    return <div className="App">Reader</div>;
   }
 }
 
