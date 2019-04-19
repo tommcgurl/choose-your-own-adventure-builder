b51d4a70ed36287d180209e1d50eea885738442b
Andrew Boza <andrew.m.boza@gmail.com>
2019-04-19 19:56:35 -0400

    Enables Redux DevTools

--- reader/src/store/configureStore.js
+++ reader/src/store/configureStore.js
@@ -2,8 +2,8 @@ import { createStore, compose } from 'redux';
 import rootReducer from '../reducers';
 import { install } from 'redux-loop';
 
-const enhancer = compose(install());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, enhancer);
+  return createStore(rootReducer, initialState, composeEnhancers(install()));
 }
