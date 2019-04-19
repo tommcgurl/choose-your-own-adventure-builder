9c169fc3446fa3c5eb03abe992ecf1d709cafef4
Tom McGurl <thomas@chartbeat.com>
2019-04-19 13:30:08 -0400

    Add crappy looking inventory selection screen that is shown if the JSON contains information about possible items.

---
+++ reader/src/components/InventorySelection/InventorySelection.js
@@ -0,0 +1,73 @@
+import React, { PureComponent, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './InventorySelection.module.css';
+
+export default class InventorySelection extends PureComponent {
+
+  state = {
+    selectedOptions: {},
+  }
+
+  static propTypes = {
+    prompt: PropTypes.string.isRequired,
+    options: PropTypes.object.isRequired,
+    onInventorySelectionComplete: PropTypes.func.isRequired,
+    limit: PropTypes.number.isRequired,
+  }
+
+  handleClickOption = (optionKey) => {
+    const newSelectedOptions = {...this.state.selectedOptions};
+    const {
+      limit,
+      onInventorySelectionComplete,
+    } = this.props;
+
+    if (newSelectedOptions[optionKey]) {
+      delete newSelectedOptions[optionKey]
+    } else {
+      newSelectedOptions[optionKey] = true;
+    }
+
+    if (Object.keys(newSelectedOptions).length == limit) {
+      onInventorySelectionComplete(newSelectedOptions)
+    } else {
+      this.setState({
+        selectedOptions: newSelectedOptions
+      });
+    }
+  }
+
+  render() {
+    const {
+      prompt ,
+      options,
+      limit,
+    } = this.props;
+    const { selectedOptions } = this.state
+    const currentSelectedOptionCount = Object.keys(selectedOptions).length;
+    return (
+      <div className={styles.container}>
+        <h1>{prompt}</h1>
+        {Object.keys(options).map(optionKey => {
+          const option = options[optionKey];
+          const selected = selectedOptions[optionKey];
+          return (
+            <div
+              className={styles.option}
+              onClick={this.handleClickOption.bind(null, optionKey)} >
+              
+              <h1>
+                {optionKey}
+                {selected && <span> ✅</span>}
+               </h1>
+              <p>{option.description}</p>
+            </div>
+          );
+        })}
+        <h4>{`${currentSelectedOptionCount}/${limit}`}</h4>
+      </div>
+    )
+  }
+
+}
--- reader/src/components/StoryManager/StoryManager.js
+++ reader/src/components/StoryManager/StoryManager.js
@@ -2,12 +2,18 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Intro from '../Intro/Intro';
 import StoryParts from '../StoryParts/StoryParts';
+import InventorySelection from '../InventorySelection/InventorySelection';
 
 import styles from './StoryManager.module.css';
 
 export default class StoryManager extends PureComponent {
-  state = {
-    introCompleted: false,
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      introCompleted: false,
+      inventorySelectionCompleted: !Object.keys(props.items).length,
+    }
   }
 
   static propTypes = {
@@ -18,6 +24,7 @@ export default class StoryManager extends PureComponent {
       options: PropTypes.shape({
         description: PropTypes.string,
       }),
+      limit: PropTypes.number,
     }),
     mainStory: PropTypes.object,
     colorPalette: PropTypes.object,
@@ -29,17 +36,36 @@ export default class StoryManager extends PureComponent {
     });
   }
 
+  handleInventorySelectionComplete = (selectedItems) => {
+    this.setState({
+      inventorySelectionCompleted: true,
+    });
+  }
+
   render() {
+    const { items } = this.props;
+    const {
+      inventorySelectionCompleted,
+      introCompleted,
+    } = this.state;
     return (
       <div className={styles.container}>
-        {!this.state.introCompleted && 
+        {!introCompleted &&
           <Intro
             title={this.props.title}
             intro={this.props.intro}
             onIntroComplete={this.handleIntroComplete}
           />
         }
-        {this.state.introCompleted &&
+        {introCompleted && !inventorySelectionCompleted && Object.keys(items).length &&
+          <InventorySelection
+            prompt={items.prompt}
+            options={items.options}
+            limit={items.limit}
+            onInventorySelectionComplete={this.handleInventorySelectionComplete}
+          />
+        }
+        {this.state.introCompleted && inventorySelectionCompleted &&
           <StoryParts
             firstPart={this.props.mainStory.firstPart}
             storyParts={this.props.mainStory.storyParts}
