90b4bbc98457371b14ab685fcb089a757cb93280
Tom McGurl <thomas@chartbeat.com>
2019-04-21 16:47:47 -0700

    Add redux-logger middleware for viewing redux store changes in devtools when in dev mode.

--- reader/src/store/configureStore.js
+++ reader/src/store/configureStore.js
@@ -1,9 +1,13 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import { install } from 'redux-loop';
+import logger from 'redux-logger';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, composeEnhancers(install()));
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(install(), applyMiddleware(logger)))
 }
