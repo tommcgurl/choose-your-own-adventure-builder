7ee7b1023f97cfe853c84ab0869eb68f37278398
Tom McGurl <thomas@chartbeat.com>
2019-04-21 17:09:09 -0700

    Adding condition to only apply logger middlware when NODE_ENV is not production.

--- reader/src/store/configureStore.js
+++ reader/src/store/configureStore.js
@@ -1,13 +1,27 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import { install } from 'redux-loop';
-import logger from 'redux-logger';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
+
+  // Set up any middleware that should always be applied.
+  let middleware = [];
+
+  // Only require and apply development middlware when not
+  // running in production mode.
+  if (process.env.NODE_ENV !== 'production' && process.env.REACT_APP_NODE_ENV !== 'production') {
+    const logger = require('redux-logger').default;
+    middleware = [...middleware, logger];
+  } else {
+    // Apply all production specific middlware here.
+    middleware = [...middleware];
+  }
+
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(install(), applyMiddleware(logger)))
+    composeEnhancers(install(), applyMiddleware(...middleware))
+  );
 }
