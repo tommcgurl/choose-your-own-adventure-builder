87f90f89396011c82e3f3d78a7881cf4e3c04966
Tom McGurl <thomas@chartbeat.com>
2019-04-21 17:11:05 -0700

    [Editor] Adding redux-logger middleware.

--- editor/src/store/configureStore.js
+++ editor/src/store/configureStore.js
@@ -1,9 +1,27 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import { install } from 'redux-loop';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, composeEnhancers(install()));
+
+  // Set up any middleware that should always be applied.
+  let middleware = [];
+
+  // Only require and apply development middlware when not
+  // running in production mode.
+  if (process.env.NODE_ENV !== 'production' && process.env.REACT_APP_NODE_ENV !== 'production') {
+    const logger = require('redux-logger').default;
+    middleware = [...middleware, logger];
+  } else {
+    // Apply all production specific middlware here.
+    middleware = [...middleware];
+  }
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(install(), applyMiddleware(...middleware))
+  );
 }
