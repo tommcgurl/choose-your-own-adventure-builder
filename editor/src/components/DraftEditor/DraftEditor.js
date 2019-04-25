e983224fa53a9f92092804f2acd5e54a9957b499
Andrew Boza <andrew.m.boza@gmail.com>
2019-04-24 20:57:11 -0400

    adds default wysiwyg editor

--- editor/src/components/DraftEditor/DraftEditor.js
+++ editor/src/components/DraftEditor/DraftEditor.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './DraftEditor.modules.css';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 const DraftEditor = props => {
-  return <div />;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  return (
+    <Editor editorState={editorState} onEditorStateChange={setEditorState} />
+  );
 };
 
 export default DraftEditor;
