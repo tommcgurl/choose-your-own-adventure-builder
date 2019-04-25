b79adc0c04d09e3a3bec7c3e9e52312ad67a5291
Andrew Boza <andrew.m.boza@gmail.com>
2019-04-24 21:43:12 -0400

    Adds styling

--- editor/src/components/DraftEditor/DraftEditor.js
+++ editor/src/components/DraftEditor/DraftEditor.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from './DraftEditor.modules.css';
+import styles from './DraftEditor.module.css';
 import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
@@ -7,7 +7,12 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 const DraftEditor = props => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
   return (
-    <Editor editorState={editorState} onEditorStateChange={setEditorState} />
+    <Editor
+      editorState={editorState}
+      onEditorStateChange={newState => setEditorState(newState)}
+      wrapperClassName={styles.wrapper}
+      editorClassName={styles.editor}
+    />
   );
 };
 
