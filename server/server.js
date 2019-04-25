50d74494ee647d650f0a32ff81eaf0607b01ca98
Warpfox <tylermwatts@gmail.com>
2019-04-24 20:46:53 -0500

    extremely simple user auth working

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const cors = require("cors");
+const users = require("./mock_data/mockUsers"); // straight up importing mock users because fuck iiiiiiiit
+
 require("dotenv").config();
 
 const app = express();
@@ -6,15 +11,33 @@ const adventuresRouter = require("./routes/adventures");
 
 const port = process.env.PORT || 8000;
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
+
+passport.use(
+  new LocalStrategy(
+    { passReqToCallback: true },
+    (req, username, password, done) => {
+      // checking the mock user array. This will likely be replaced with a call to a separate UserRepository, similar to AdventureRepository
+      let user = users.filter(u => u.username === username)[0];
+      return user
+        ? user.password === password
+          ? done(null, user)
+          : done(null, false, { message: "Incorrect Password" })
+        : done(null, false, { message: "Incorrect Username" });
+    }
+  )
+);
+
+app.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    console.log(req.body);
+    res.json({ message: "Login successful" });
+  }
+);
 app.use("/adventures", adventuresRouter);
 
 app.listen(port, () => {
