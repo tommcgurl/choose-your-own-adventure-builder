8bf57ddf4fcb45f0b52860daf27882a4774daac3
Andrew Boza <andrew.m.boza@gmail.com>
2019-04-27 13:07:51 -0400

    [editor] only allows devtools when not in prod mode

--- editor/src/store/configureStore.js
+++ editor/src/store/configureStore.js
@@ -2,18 +2,22 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import { install } from 'redux-loop';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 export default function configureStore(initialState) {
+  let composeEnhancers = compose;
 
   // Set up any middleware that should always be applied.
   let middleware = [];
 
   // Only require and apply development middlware when not
   // running in production mode.
-  if (process.env.NODE_ENV !== 'production' && process.env.REACT_APP_NODE_ENV !== 'production') {
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    process.env.REACT_APP_NODE_ENV !== 'production'
+  ) {
     const logger = require('redux-logger').default;
     middleware = [...middleware, logger];
+    composeEnhancers =
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeEnhancers;
   } else {
     // Apply all production specific middlware here.
     middleware = [...middleware];
@@ -22,6 +26,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(install(), applyMiddleware(...middleware))
+    composeEnhancers(install(), applyMiddleware(...middleware)),
   );
 }
