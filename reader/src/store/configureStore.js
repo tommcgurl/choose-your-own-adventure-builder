1e01d08b9fe79c05d9747e457ee717b0ddfdb89d
Andrew Boza <andrew.m.boza@gmail.com>
2019-04-27 13:07:36 -0400

    [reader] only allows devtools when not in prod mode

--- reader/src/store/configureStore.js
+++ reader/src/store/configureStore.js
@@ -1,20 +1,24 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import { install } from 'redux-loop';
-import persistMiddlware from '../middleware/persistStoreMiddleware';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import persistMiddleware from '../middleware/persistStoreMiddleware';
 
 export default function configureStore(initialState = {}) {
+  let composeEnhancers = compose;
 
   // Set up any middleware that should always be applied.
-  let middleware = [persistMiddlware];
+  let middleware = [persistMiddleware];
 
   // Only require and apply development middlware when not
   // running in production mode.
-  if (process.env.NODE_ENV !== 'production' && process.env.REACT_APP_NODE_ENV !== 'production') {
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    process.env.REACT_APP_NODE_ENV !== 'production'
+  ) {
     const logger = require('redux-logger').default;
     middleware = [...middleware, logger];
+    composeEnhancers =
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeEnhancers;
   } else {
     // Apply all production specific middlware here.
     middleware = [...middleware];
@@ -23,6 +27,6 @@ export default function configureStore(initialState = {}) {
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(install(), applyMiddleware(...middleware))
+    composeEnhancers(install(), applyMiddleware(...middleware)),
   );
 }
