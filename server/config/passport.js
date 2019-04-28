87030293fc9a6a6c55c59df0a16b953a400f143c
Warpfox <tylermwatts@gmail.com>
2019-04-28 17:39:52 -0500

    comments in the passport config

--- server/config/passport.js
+++ server/config/passport.js
@@ -14,14 +14,18 @@ module.exports = function configPassport(app) {
     new LocalStrategy(
       { passReqToCallback: true },
       (req, username, password, done) => {
+        // vvv Searching the mock user database to see if a match is found
         let user = userRepository
           .getUsers()
           .filter(u => u.username === username)[0];
+        // vvv If the user exists, checks the password - if both checkout, returns the user object from the database
         return user
           ? user.password === password
             ? done(null, user)
-            : done(null, false, { message: "Incorrect Password." })
-          : done(null, false, { message: "That username does not exist." });
+            : // vvv If the user exists but the password is incorrect
+              done(null, false, { message: "Incorrect Password." })
+          : // vvv If the user is not found in the database
+            done(null, false, { message: "That username does not exist." });
       }
     )
   );
@@ -37,7 +41,8 @@ module.exports = function configPassport(app) {
         let user = userRepository
           .getUsers()
           .filter(u => u.username === profile.displayName)[0];
-        return user // If the user exists in the database, send it back to the authentication handler, otherwise send back a new user object
+        // vvv If the user exists in the "database", this sends the user object back to the authentication handler, otherwise returns done() with a new user object
+        return user
           ? done(null, user)
           : done(null, {
               id: userRepository.getUsers().length + 1,
