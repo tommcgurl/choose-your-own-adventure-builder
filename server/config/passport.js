1e5f27123afb0dd02c34b92e926e093f392189ce
Andrew Boza <andrew.m.boza@gmail.com>
2019-05-10 21:45:57 -0400

    [server] cleans up passport config

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
-const userRepository = require("../repositories/UserRepository");
+require('dotenv').config();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const userRepository = require('../repositories/UserRepository');
 
 const OAUTH_CLIENT_ID = process.env.OAUTH_CLIENT_ID;
 const OAUTH_CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET;
@@ -13,75 +13,45 @@ const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 module.exports = function configPassport(app) {
   app.use(passport.initialize());
 
-  passport.use(
-    new LocalStrategy(
-      { passReqToCallback: true },
-      (req, username, password, done) => {
-        // vvv Searching the mock user database to see if a match is found
-        let user = userRepository
-          .getUsers()
-          .filter(u => u.username === username)[0];
-        // vvv If the user exists, checks the password - if both checkout, returns the user object from the database
-        return user
-          ? user.password === password
-            ? done(null, user)
-            : // vvv If the user exists but the password is incorrect
-              done(null, false, { message: "Incorrect Password." })
-          : // vvv If the user is not found in the database
-            done(null, false, { message: "That username does not exist." });
+  const handleAuthentication = (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = userRepository.getUserByProviderId(
+        profile.provider,
+        profile.id,
+      );
+      if (!user) {
+        user = userRepository.createUser(
+          profile.provider,
+          profile.id,
+          profile.displayName,
+        );
       }
-    )
-  );
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  };
 
   passport.use(
     new GoogleStrategy(
       {
-        callbackURL: "/auth/google/redirect",
+        callbackURL: '/auth/google/redirect',
         clientID: OAUTH_CLIENT_ID,
-        clientSecret: OAUTH_CLIENT_SECRET
+        clientSecret: OAUTH_CLIENT_SECRET,
       },
-      (accessToken, refreshToken, profile, done) => {
-        console.log(`accessToken: ${accessToken}`);
-        console.log(`refreshToken: ${refreshToken}`);
-        try {
-          let user = userRepository.getUserByProviderId(
-            profile.provider,
-            profile.id
-          );
-          if (!user) {
-            user = userRepository.createUser(
-              profile.provider,
-              profile.id,
-              profile.displayName
-            );
-          }
-          done(null, user);
-        } catch (err) {
-          done(err);
-        }
-      }
-    )
+      handleAuthentication,
+    ),
   );
 
   passport.use(
     new FacebookStrategy(
       {
-        callbackURL: "/auth/facebook/redirect",
+        callbackURL: '/auth/facebook/redirect',
         clientID: FACEBOOK_APP_ID,
-        clientSecret: FACEBOOK_APP_SECRET
+        clientSecret: FACEBOOK_APP_SECRET,
       },
-      (accessToken, refreshToken, profile, done) => {
-        let user = userRepository
-          .getUsers()
-          .filter(u => u.username === profile.displayName)[0];
-        return user
-          ? done(null, user)
-          : done(null, {
-              id: userRepository.getUsers().length + 1,
-              username: profile.displayName
-            });
-      }
-    )
+      handleAuthentication,
+    ),
   );
 
   return passport;
