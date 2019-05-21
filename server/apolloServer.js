887151175c894e66eb918dc7a79d591d05e5880b
Andrew Boza <andrew.m.boza@gmail.com>
2019-05-21 19:04:45 -0400

    [server] fixes apolloServer context

--- server/apolloServer.js
+++ server/apolloServer.js
@@ -4,14 +4,17 @@ const typeDefs = require('./schema/typeDefs');
 const resolvers = require('./schema/resolvers');
 
 const context = ({ req }) => {
-  const token = (req.headers && req.headers.authorization) || '';
-  try {
-    const parsedToken = parseToken(token.substring(7));
-    return { user: parsedToken };
-  } catch(err) {
-    console.error(err)
-    return {};
+  const token = req.headers && req.headers.authorization;
+  if (token) {
+    try {
+      const parsedToken = parseToken(token.substring(7));
+      return { user: parsedToken };
+    } catch (err) {
+      console.error(err);
+    }
   }
+
+  return {};
 };
 
 module.exports = new ApolloServer({ typeDefs, resolvers, context });
