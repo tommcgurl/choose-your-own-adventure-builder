21d2d23393d534d804a7c074b73c80d2c6acdb6a
Andrew Boza <andrew.m.boza@gmail.com>
2019-05-26 14:22:26 -0400

    [server] adds helpful comments to wrapped node-postgres Pool

--- server/db/index.js
+++ server/db/index.js
@@ -3,8 +3,16 @@ const { Pool } = require('pg');
 const pool = new Pool();
 
 module.exports = {
+  /**
+   * Only to be used for single queries (not transactions).
+   * https://node-postgres.com/features/pooling#single-query
+   */
   query: async (text, params) => {
     return await pool.query(text, params);
   },
+  /**
+   * Use the returned "PoolClient" object to query the database. Must wrap a call to PoolClient.release() in a finally block.
+   * https://node-postgres.com/features/transactions#a-pooled-client-with-async-await
+   */
   connect: async () => await pool.connect(),
 };
