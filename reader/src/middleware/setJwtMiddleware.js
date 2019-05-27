b60c09739b30d6febd06d05d499f3e23cabb7df2
Andrew Boza <andrew.m.boza@gmail.com>
2019-05-27 10:26:57 -0400

    [editor, reader] Nulls out apolloClient token on logOut

--- reader/src/middleware/setJwtMiddleware.js
+++ reader/src/middleware/setJwtMiddleware.js
@@ -1,9 +1,11 @@
-import { AUTHENTICATED } from '../constants/actionTypes';
+import { AUTHENTICATED, LOG_OUT } from '../constants/actionTypes';
 import apolloClient from '../services/apolloClient';
 
 const setJwtMiddleware = store => next => action => {
   if (action.type === AUTHENTICATED) {
     apolloClient.setToken(action.token);
+  } else if (action.type === LOG_OUT) {
+    apolloClient.setToken(null);
   }
 
   next(action);
