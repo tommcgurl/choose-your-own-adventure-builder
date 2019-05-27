8c047e5a48bd85fa40ecc19a2b31e5845149067b
Andrew Boza <andrew.m.boza@gmail.com>
2019-05-27 15:01:49 -0400

    Stores EditorState as RawContentState in localForage

--- client/src/shared/services/StatePersistenceService.js
+++ client/src/shared/services/StatePersistenceService.js
@@ -1,15 +1,26 @@
+import { convertFromRaw, convertToRaw, EditorState } from 'draft-js';
 import localforage from 'localforage';
 import { STORAGE_ID } from '../constants';
 
 export default class StatePersistenceService {
   static async perisistState(state) {
-    await localforage.setItem(STORAGE_ID, state);
+    const storageFriendlyState = {
+      ...state,
+      editor: {
+        ...state.editor,
+        editor: {
+          ...state.editor.editor,
+          state: convertToRaw(state.editor.editor.state.getCurrentContent()),
+        },
+      },
+    };
+    await localforage.setItem(STORAGE_ID, storageFriendlyState);
     try {
       const persistedState = await localforage.getItem(STORAGE_ID);
       if (persistedState) {
         console.log(
           '%cCurrent progress successfully saved.',
-          'color: green; font-weight: 600',
+          'color: green; font-weight: 600'
         );
       } else {
         throw new Error();
@@ -21,6 +32,19 @@ export default class StatePersistenceService {
 
   static async getPersistedState() {
     const persistedState = await localforage.getItem(STORAGE_ID);
-    return persistedState;
+    if (persistedState) {
+      return {
+        ...persistedState,
+        editor: {
+          ...persistedState.editor,
+          editor: {
+            ...persistedState.editor.editor,
+            state: EditorState.createWithContent(
+              convertFromRaw(persistedState.editor.editor.state)
+            ),
+          },
+        },
+      };
+    }
   }
 }
