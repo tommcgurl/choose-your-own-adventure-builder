20d3a06cc006cd12386660342334ef194c352972
Warpfox <tylermwatts@gmail.com>
2019-05-28 20:11:29 -0500

    add convenience buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NotFound from './shared/components/NotFound';
 
 const EditorApp = lazy(() => import('./editor/App'));
 const ReaderApp = lazy(() => import('./reader/App'));
 
 const App = props => {
+  const ConvenienceLinks = ({ history }) => {
+    return (
+      <>
+        <button onClick={() => history.push('/reader')}>
+          {'Go To Reader'}
+        </button>
+        <button onClick={() => history.push('/editor')}>
+          {'Go To Editor'}
+        </button>
+      </>
+    );
+  };
+
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route path="/reader" component={ReaderApp} />
           <Route path="/editor" component={EditorApp} />
-          <Route component={NotFound} />
+          <Route render={props => <ConvenienceLinks {...props} />} />
         </Switch>
       </Suspense>
     </Router>
