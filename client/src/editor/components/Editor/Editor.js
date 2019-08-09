c4d546efa27d97f9b390e7b9ea437aaf8d46e3b9
Warpfox <tylermwatts@gmail.com>
2019-08-08 21:11:58 -0500

    displays new story key immediately after edit

--- client/src/editor/components/Editor/Editor.js
+++ client/src/editor/components/Editor/Editor.js
@@ -150,7 +150,7 @@ const Editor = ({
             'Intro'
           ) : (
             <>
-              {storyPartKey}
+              {newStoryPartKey}
               <button onClick={handleNewStoryPartKeyEditClick}>Edit</button>
             </>
           )}
