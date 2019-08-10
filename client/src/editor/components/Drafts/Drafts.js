47f57732ae54175291bd5f390a6ea2b15306baa2
Warpfox <tylermwatts@gmail.com>
2019-08-10 09:31:42 -0500

    small refactor

--- client/src/editor/components/Drafts/Drafts.js
+++ client/src/editor/components/Drafts/Drafts.js
@@ -5,19 +5,16 @@ import * as routes from '../../constants/routes';
 import { draftsSelector } from '../../store/selectors';
 
 const Drafts = ({ drafts }) => {
-  return (
-    <ul>
-      {drafts.map(draft => {
-        return (
-          <li key={draft.id}>
-            <Link to={routes.DRAFT.replace(':draftId', draft.id)}>
-              {draft.title}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
+  const draftLink = draft => {
+    return (
+      <li key={draft.id}>
+        <Link to={routes.DRAFT.replace(':draftId', draft.id)}>
+          {draft.title}
+        </Link>
+      </li>
+    );
+  };
+  return <ul>{drafts.map(draftLink)}</ul>;
 };
 
 const mapStateToProps = state => {
--- client/src/editor/components/PublishedAdventures/PublishedAdventures.js
+++ client/src/editor/components/PublishedAdventures/PublishedAdventures.js
@@ -5,24 +5,19 @@ import * as routes from '../../constants/routes';
 import { publishedAdventuresSelector } from '../../store/selectors';
 
 const PublishedAdventures = ({ adventures }) => {
-  return (
-    <ul>
-      {adventures.map(adventure => {
-        return (
-          <li key={adventure.id}>
-            <Link
-              to={routes.PUBLISHED_ADVENTURE.replace(
-                ':adventureId',
-                adventure.id
-              )}
-            >
-              {adventure.title}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
+  const publishedAdventureLink = adventure => {
+    return (
+      <li key={adventure.id}>
+        <Link
+          to={routes.PUBLISHED_ADVENTURE.replace(':adventureId', adventure.id)}
+        >
+          {adventure.title}
+        </Link>
+      </li>
+    );
+  };
+
+  return <ul>{adventures.map(publishedAdventureLink)}</ul>;
 };
 
 const mapStateToProps = state => {
