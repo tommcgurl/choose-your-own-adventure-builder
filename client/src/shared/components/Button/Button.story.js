8dcb7008feeeea7836202550595aa82750deb9de
Tom McGurl <thomas@chartbeat.com>
2019-08-20 08:02:36 -0400

    Adding Storybook 5.1 and a story for the Button component.

---
+++ client/src/shared/components/Button/Button.story.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Button, { TYPES } from './Button';
+
+storiesOf('Components|Button', module)
+  .add('default', () => (
+    <Button>default button!</Button>
+  ))
+  .add('destructive', () => (
+    <Button type={TYPES.DESTRUCTIVE}>
+      destructive button!
+    </Button>
+  ))
+  .add('action', () => (
+    <Button type={TYPES.ACTION}>
+      action button!
+    </Button>
+  ))
