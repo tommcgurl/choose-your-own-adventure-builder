8c9c217c6673240b31c09718fe67d287b3e37029
Warpfox <tylermwatts@gmail.com>
2019-08-25 08:33:13 -0500

    moves click away demo into separate story

--- client/src/shared/components/Modal/Modal.story.js
+++ client/src/shared/components/Modal/Modal.story.js
@@ -107,6 +107,29 @@ const GenreDescriptionModalWrapper = ({ genre, clickAwayEnabled }) => {
   );
 }
 
+const ClickAwayModalWrapper = ({ clickAwayEnabled }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <div>
+        <Button onClick={toggleModal}>Open Modal</Button>
+      </div>
+      <Modal
+        clickAwayEnabled={clickAwayEnabled}
+        isOpen={isOpen}
+        closeModal={toggleModal} >
+        <div className="click-away-container">
+          <h1>Title</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
 const horror = {
   title: "Horror",
   description: "The Horror genre is replete with tales of the macabre. Oftentimes supernatural in nature, the purpose of these adventures is to invoke terror in the reader. Whether you are being chased by an insane killer, haunted by a vengeful poltergeist, or possessed by a demon, adventures in the Horror genre ratchet up tension with each choice you make and use your fear against you. Common elements include: blood/gore/body horror; ghosts, poltergeists, or demons; zombies, werewolves, vampires, and other supernatural beings."
@@ -116,6 +139,11 @@ const action_adventure = {
   title: "Action/Adventure",
   description: "Action/Adventure stories typically involve the larger than life exploits of an individual or team of specialists. Plots may have fantastical elements but are always grounded in reality. Common themes include: covert military operations; treasure hunting; daring escapes/breakouts; gun fighting and/or explosions; plane/train/automobile chases."
 }
+
+storiesOf('Components|Modal/Click Away To Close')
+  .addParameters({ component: Modal })
+  .add('Enabled', () => <ClickAwayModalWrapper clickAwayEnabled={true} />)
+  .add('Disabled', () => <ClickAwayModalWrapper clickAwayEnabled={false} />)
 
 storiesOf('Components|Modal/Editor Interface', module)
   .addParameters({ component: Modal })
@@ -139,11 +167,4 @@ storiesOf('Components|Modal/Genre Description')
   .add('Action/Adventure', ({ genre = action_adventure }) => {
     return <GenreDescriptionModalWrapper genre={genre} />
   })
-  .add('Click Away to Close Disabled', ({ genre = action_adventure }) => {
-    return (
-      <GenreDescriptionModalWrapper
-        clickAwayEnabled={false}
-        genre={genre}
-      />
-    )
-  })
+
