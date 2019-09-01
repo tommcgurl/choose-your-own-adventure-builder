ea67a5c257df80c23ccf5570132b7f86cb8c96f9
Andrew Boza <andrew.m.boza@gmail.com>
2019-09-01 09:42:14 -0400

    WIP it good

--- client/src/shared/components/Toast/Notification/Notification.js
+++ client/src/shared/components/Toast/Notification/Notification.js
@@ -1,30 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './Notification.module.css';
 
-const Notification = ({ children, onHideComplete }) => {
-  // const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    // const showTimeout = setTimeout(() => {
-    //   setShow(true);
-    // }, 0);
-
-    // const hideTimeout = setTimeout(() => {
-    //   setShow(false);
-    // }, 1000);
-
-    const hideCompleteTimeout = setTimeout(() => {
-      // onHideComplete();
-    }, 1250);
-
-    return () => {
-      // clearTimeout(showTimeout);
-      // clearTimeout(hideTimeout);
-      clearTimeout(hideCompleteTimeout);
-    };
-  });
-
-  return <div className={styles.container}>{children}</div>;
+const Notification = ({ children, ...rest }) => {
+  return (
+    <div className={styles.container} {...rest}>
+      {children}
+    </div>
+  );
 };
 
 export default Notification;
--- client/src/shared/components/Toast/Toast.js
+++ client/src/shared/components/Toast/Toast.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import styles from './Toast.module.css';
-import eventManager from '../../services/eventManager';
+import React, { useCallback, useEffect, useState } from 'react';
+import { animated, useTransition } from 'react-spring';
 import uuid from 'uuid/v4';
+import eventManager from '../../services/eventManager';
 import Notification from './Notification';
+import styles from './Toast.module.css';
+
+const AnimatedNotification = animated(Notification);
 
 export const VARIANTS = {
   INFORMATIONAL: 'INFORMATIONAL',
@@ -13,8 +16,12 @@ export const toast = message => {
   eventManager.emit('pop', message);
 };
 
-const Toast = () => {
+const Toast = ({
+  // config = { tension: 125, friction: 20, precision: 0.1 },
+  delay = 2000,
+}) => {
   const [notifications, setNotifications] = useState([]);
+  const [timeouts, setTimeouts] = useState([]);
   useEffect(() => {
     function handlePop(message) {
       const notification = { id: uuid(), message };
@@ -26,16 +33,38 @@ const Toast = () => {
     };
   });
 
-  function handleNotificationComplete(id) {
-    setNotifications(notifications.filter(n => n.id !== id));
-  }
+  const cleanupTimeouts = useCallback(() => {
+    timeouts.forEach(timeout => {
+      clearTimeout(timeout);
+    });
+  }, [timeouts]);
+
+  useEffect(() => {
+    return cleanupTimeouts;
+  }, []);
+
+  const transitions = useTransition(notifications, item => item.id, {
+    from: { opacity: 0 },
+    enter: item => async next => await next({ opacity: 1 }),
+    leave: item => async (next, cancel) => {
+      await next({ opacity: 0 });
+    },
+    onRest: item => {
+      const timeout = setTimeout(() => {
+        setNotifications(state => state.filter(n => n.id !== item.id));
+      }, delay);
+      setTimeouts([...timeouts, timeout]);
+    },
+    // config: (item, state) =>
+    //   state === 'leave' ? [{ duration: delay }, config, config] : config,
+  });
 
   return (
     <div className={styles.container}>
-      {notifications.map(notification => (
-        <Notification key={notification.id}>
-          {notification.message}
-        </Notification>
+      {transitions.map(({ key, item, props }) => (
+        <AnimatedNotification key={key} style={props}>
+          {item.message}
+        </AnimatedNotification>
       ))}
     </div>
   );
