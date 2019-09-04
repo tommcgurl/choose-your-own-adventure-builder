086045043fb72c2cdb1f173685fae0d2166e49ea
Tom McGurl <thomas@chartbeat.com>
2019-09-03 21:42:31 -0400

    Adding Cypress and some basic tests

---
+++ client/cypress/integration/Reader.spec.js
@@ -0,0 +1,14 @@
+describe('Reader', function () {
+
+  it('loads the reader', function () {
+    // cy.server();
+    // intercepts GET requests to this url and sends back fixture as response
+    // cy.route('**/reports/api/get-subscriptions-for-user/**', 'fixture:get-subscriptions-for-user.json');
+
+    cy.visit('http://localhost:3000/');
+    cy.get('#reader-button').click();
+    cy.get('#top-navigation').should('be.visible');
+
+    // cy.get('[data-testid=reports]').should('be.visible');
+  })
+})
