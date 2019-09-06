1ce1cd4003cb0964854cce3e3b9b2e5aaecf1a0b
Warpfox <tylermwatts@gmail.com>
2019-09-05 21:14:47 -0500

    small testing changes

--- client/cypress/integration/Reader.spec.js
+++ client/cypress/integration/Reader.spec.js
@@ -1,16 +1,14 @@
-describe('Reader', function () {
-
-  it('loads the reader', function () {
-
+describe('Reader', function() {
+  it('loads the reader', function() {
     cy.visit('http://localhost:3000/');
     cy.get('#reader-button').click();
     cy.get('#top-navigation').should('be.visible');
     // cy.get('')
 
     // cy.get('[data-testid=reports]').should('be.visible');
-  })
+  });
 
-  it('loads the adventures', function () {
+  it('loads the adventures', function() {
     cy.server();
     // intercepts GET requests to this url and sends back fixture as response
     cy.route('**/graphql/**', 'fixture:stories.json');
@@ -18,5 +16,14 @@ describe('Reader', function () {
     const adventureList = cy.get('#adventure-list');
     adventureList.should('be.visible');
     adventureList.contains('ul');
-  })
-})
+  });
+
+  it('has a searchbar', function() {
+    cy.get('#adventure-search').should('be.visible');
+  });
+
+  it('loads the FAQ page', function() {
+    cy.visit('http://localhost:3000/reader/faq');
+    cy.get('#faq-h1').should('be.visible');
+  });
+});
