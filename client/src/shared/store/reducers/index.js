eb7df1465fcbb0fe914ec630a9b801b24abfe158
Andrew Boza <andrew.m.boza@gmail.com>
2019-09-12 21:23:16 -0400

    Fixes shared root reducer

--- client/src/shared/store/reducers/index.js
+++ client/src/shared/store/reducers/index.js
@@ -3,12 +3,14 @@ import editorReducer from '../../../editor/store/reducers';
 import readerReducer from '../../../reader/store/reducers';
 import authReducer from './authReducer';
 import listReducer from './listReducer';
+import userSettingsReducer from './userSettingsReducer';
 
 const rootReducer = combineReducers({
   reader: readerReducer,
   editor: editorReducer,
   token: authReducer,
   lists: listReducer,
+  userSettings: userSettingsReducer,
 });
 
 export default rootReducer;
