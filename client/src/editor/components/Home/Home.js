1f3ea1f051fdf7edabaa571df51df809da17b69e
tylermwatts <tylermwatts@gmail.com>
2019-11-21 21:07:19 -0600

    wip

--- client/src/editor/components/Home/Home.js
+++ client/src/editor/components/Home/Home.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Box, Inline, Stack, Columns } from '../../../shared/components';
+import * as styles from './Home.module.css';
+import Drafts from '../Drafts/Drafts';
+import PublishedAdventures from '../PublishedAdventures/PublishedAdventures';
 
 const Home = props => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <Box shadow className={styles.titleContainer}>
+        <Inline align="center">
+          <h1 className={styles.title}>Embark</h1>
+        </Inline>
+        <Inline align="center">
+          <h2>A "choose your own adventure" app</h2>
+        </Inline>
+      </Box>
+      <Stack align="center">
+        <Box shadow className={styles.module}>
+          <h3>Your Drafts</h3>
+          <Inline style={{ textAlign: 'left' }} align="left">
+            <Drafts />
+          </Inline>
+        </Box>
+        <Box shadow className={styles.module}>
+          <h3>Your Published Adventures</h3>
+          <Inline align="left">
+            <PublishedAdventures />
+          </Inline>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Home;
